86a0cc36cd47aec9f90bc4b0378b63e84dddf1c4
Timothy Spinney <tspinney@seattleu.edu>
2024-05-22 17:15:16 -0700

    fetching menu data from proxy server

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
 import MenuSection from './MenuSection'
-import { mockData } from '../mockData'
+import { MenuSection as MenuSectionModel } from '../model/MenuSection'
 
 export default function Menu() {
+    const [menuData, setMenuData] = useState<MenuSectionModel[]>()
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const getData = async () => {
+            const response = await fetch('/api/menu/items', {
+                signal: controller.signal
+            })
+            const data = await response.json()
+            setMenuData(data)
+        }
+        getData()
+        return () => {
+            controller.abort()
+        }
+    }, [])
+
+    if(!menuData) {
+        return <div>Loading&hellip;</div>
+    }
+
     return (
         <>
-          {mockData.map(
+          {menuData.map(
               (menuSectionModel) => <MenuSection data={menuSectionModel} />
           )}
         </>
